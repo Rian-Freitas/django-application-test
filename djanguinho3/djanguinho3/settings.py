1f32d06581ccdf8f6ff0a893adbfdee7f94d47ca
LucasTreuke <lucasbryantreuke@gmail.com>
2021-10-29 09:07:35 -0300

    Mudado o caminho para a templates

--- djanguinho3/djanguinho3/settings.py
+++ djanguinho3/djanguinho3/settings.py
@@ -15,7 +15,7 @@ import pathlib
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATE_DIR = BASE_DIR / "templates"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'djanguinho3.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [pathlib.Path('.').parent.joinpath('templates')],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
