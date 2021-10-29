d7ecf390320c091a853a51d7aad8da3a39539c61
Lfzinho <laguardialuis@gmail.com>
2021-10-29 07:31:44 -0300

    faltando uma virgulazinha aq tbm

--- djanguinho3/dominique/urls.py
+++ djanguinho3/dominique/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from dominique import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
     path("special/", views.special, name="special")
 ]
