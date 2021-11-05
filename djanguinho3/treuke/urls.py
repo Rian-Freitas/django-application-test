e6bbb9dac64c1a8505d0d6aacb62bdc3f0012ba5
LucasTreuke <lucasbryantreuke@gmail.com>
2021-11-05 08:41:32 -0300

    Lucas - redirect

--- djanguinho3/treuke/urls.py
+++ djanguinho3/treuke/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path("", treuke_views.index, name="index"),
     path("special/", treuke_views.special, name="special"),
     path("pontos/", treuke_views.pontos, name="pontos"),
-    path("bacon/", treuke_views.bacon, name="bacon")
+    path("bacon/", treuke_views.bacon, name="bacon"),
+    path("redireciona/",treuke_views.redireciona, name="redireciona"),
+    path("special/<int:param>", treuke_views.view_dinamica_int, name="dinamica-int")
 ]
--- djanguinho3/treuke/views.py
+++ djanguinho3/treuke/views.py
@@ -1,9 +1,22 @@
 from django.http import HttpResponse
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
+from django.shortcuts import reverse
 
 # Create your views here.
 def index(request):
     return HttpResponse("<a href='special'>Special</a><br><a href='pontos'>pontos</a><br><a href='bacon'>bacon</a><br><strong>Coé</strong>")
+
+def redireciona(request):
+    url = reverse("bacon")
+    return HttpResponseRedirect(url)
+
+def view_dinamica_int(request,param):
+    if param == 0:
+        return HttpResponse("<strong>ALO</strong>")
+    elif param == 1:
+        return HttpResponse("<strong>ALO2</strong>")
+
 
 def special(request):
     context = {
