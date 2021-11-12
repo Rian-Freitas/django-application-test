d0b074e27503c37fe847478d9d378a7b6023d84f
DominiqueAzevedo <dominickanjo@gmail.com>
2021-11-12 20:26:10 -0300

    Update views.py

--- djanguinho3/dominique/views.py
+++ djanguinho3/dominique/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
 from django.shortcuts import reverse
+import random
 
 # Create your views here.
 def index(request):
@@ -20,8 +21,13 @@ def index_2(request):
     return render(request,"Domi/index_2.html")
 
 def index_3(request):
+    respostas = ["sim", "não"]
     context = {
         "nome":"Dominique",
+        "lista":["Treuke", "Laguardia", "Rodrigo", "Iara", "Rian"],
+        "resposta":random.choice(respostas),
+        "res1":"que bom!",
+        "res2":"f por você.",
     }
     return render(request,"Domi/index_3.html", context)
 
