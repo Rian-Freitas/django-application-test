3833254659fdb2176bff81c4530c5e842fa8faea
DominiqueAzevedo <dominickanjo@gmail.com>
2021-11-12 19:25:17 -0300

    Update views.py

--- djanguinho3/dominique/views.py
+++ djanguinho3/dominique/views.py
@@ -1,13 +1,30 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.http.response import HttpResponseRedirect
+from django.shortcuts import render
+from django.shortcuts import reverse
 
 # Create your views here.
 def index(request):
     return HttpResponse("<strong>AAAAAAAAAAA</strong>")
 
+def view_dinamica_int(request,param):
+    if param == 0:
+        return HttpResponse("<strong>Sei lá</strong>")
+    elif param == 1:
+        return HttpResponse("<strong>Sei lá 2</strong>")
+
 def special(request):
+    return render(request,"Domi/index.html")
+
+def index_2(request):
+    return render(request,"Domi/index_2.html")
+
+def index_3(request):
     context = {
         "nome":"Dominique",
-        "nome_familia":"Azevedo"
     }
-    return render(request,"dominique/index.html", context)
+    return render(request,"Domi/index_3.html", context)
+
+def redireciona(request):
+    url = reverse(index_3)
+    return HttpResponseRedirect(url)
